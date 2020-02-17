2c2109b4e3b6a6d0e0e51e3a5ba7281793db4230
eric <erc.hakawa@gmail.com>
2020-02-17 09:44:35 -0500

    commented

--- src/main/java/frc/robot/commands/DriverControl.java
+++ src/main/java/frc/robot/commands/DriverControl.java
@@ -21,24 +21,24 @@ public class DriverControl extends CommandBase {
     private final DoubleSupplier m_forward;
     private final DoubleSupplier m_rotation;
     private final BooleanSupplier m_seekTarget;
-    NetworkTable Lime = NetworkTableInstance.getDefault().getTable("limelight"); // The Limelight Vision system posts several useful bits
-                                                                                    // of data to Network Tables.
+    NetworkTable Lime = NetworkTableInstance.getDefault().getTable("limelight"); // The Limelight Vision system posts
+                                                                                 // several useful bits
+                                                                                 // of data to Network Tables.
     NetworkTableEntry tx = Lime.getEntry("tx"); // Horizontal Offset From Crosshair to Target (-27 to 27 degrees)
     NetworkTableEntry tv = Lime.getEntry("tv");
 
     private final double kP = -0.1; // proportional control for target seeking
-    private final double minCommand = 0.05; // apply minimum to make bot move as output approaches 0; 
+    private final double minCommand = 0.05; // apply minimum to make bot move as output approaches 0;
 
-
-  public DriverControl(Drivetrain DT, DoubleSupplier forward, DoubleSupplier rotation, BooleanSupplier seekTarget) {
-    Drive = DT;
-    m_forward = forward;
-    m_rotation = rotation;
-    m_seekTarget = seekTarget;
-    addRequirements(Drive);
+    public DriverControl(Drivetrain DT, DoubleSupplier forward, DoubleSupplier rotation, BooleanSupplier seekTarget) {
+        Drive = DT;
+        m_forward = forward;
+        m_rotation = rotation;
+        m_seekTarget = seekTarget;
+        addRequirements(Drive);
     }
 
-	// Called just before this Command runs the first time
+    // Called just before this Command runs the first time
     @Override
     public void initialize() {
     }
@@ -53,27 +53,31 @@ public class DriverControl extends CommandBase {
             quickTurn = false;
         // quickTurn = true;
 
-        if (m_seekTarget.getAsBoolean()&&tv.getDouble(0.0)>0){
+        // BANANA i dont really know what this is for (i thought the turret would be
+        // doing the moving)
+        // but in any case it would be better practice i think to make most of this a
+        // function in Drivetrain
+        // and just call the function
+        if (m_seekTarget.getAsBoolean() && tv.getDouble(0.0) > 0) {
             double x = tx.getDouble(0.0);
             double headingError = -tx.getDouble(0.0);
             double steeringAdjust = 0.0;
             double rightOutput = m_forward.getAsDouble() - m_rotation.getAsDouble();
             double leftOutput = m_forward.getAsDouble() + m_rotation.getAsDouble();
 
-            if (x > 1.0){
-                steeringAdjust = kP*headingError - minCommand;
-            }
-            else if (x < -1.0){
-                steeringAdjust = kP*headingError + minCommand;
+            if (x > 1.0) {
+                steeringAdjust = kP * headingError - minCommand;
+            } else if (x < -1.0) {
+                steeringAdjust = kP * headingError + minCommand;
             }
             leftOutput += steeringAdjust;
             rightOutput -= steeringAdjust;
             Drive.setOpenLoop(leftOutput, rightOutput);
-        }
-        else if (quickTurn) {
+        } else if (quickTurn) {
             Drive.driverControl(-m_forward.getAsDouble() * 0.5, m_rotation.getAsDouble(), quickTurn);
         } else {
-            Drive.driverControl(-m_forward.getAsDouble() * Math.abs(m_forward.getAsDouble()), m_rotation.getAsDouble() * 0.5, quickTurn);
+            Drive.driverControl(-m_forward.getAsDouble() * Math.abs(m_forward.getAsDouble()),
+                    m_rotation.getAsDouble() * 0.5, quickTurn);
         }
     }
 
