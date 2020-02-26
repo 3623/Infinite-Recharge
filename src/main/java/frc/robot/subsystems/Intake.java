487739a98e096f4357041a41b011db20b8dd4ddf
eric <erc.hakawa@gmail.com>
2020-02-26 14:38:28 -0500

    intake is now setINtaking(bool)

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,8 +8,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
@@ -17,50 +16,36 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
 /**
- * An example subsystem.  You can replace me with your own Subsystem.
+ * An example subsystem. You can replace me with your own Subsystem.
  */
 public class Intake extends SubsystemBase {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-  private VictorSPX collector;
-  private TalonSRX indexer;
+  private static final double INTAKE_SPEED = 1.0;
+  private WPI_VictorSPX collector;
 
   private Solenoid collectorDrop;
-  // private VictorSPX collectorDropper;
 
   private DigitalInput ballSensor1, ballSensor2, ballSensor3, ballSensor4, ballSensor5;
 
-  public Intake(){
-    collector = new VictorSPX(IntakeConstants.INTAKE_COLLECTOR_MOTOR_SPX);
-    indexer = new TalonSRX(IntakeConstants.INTAKE_INDEXER_MOTOR_SRX);
+  public Intake() {
+    collector = new WPI_VictorSPX(IntakeConstants.INTAKE_COLLECTOR_MOTOR_SPX);
 
     collectorDrop = new Solenoid(IntakeConstants.INTAKE_DROP_SOLENOID);
-    // collectorDropper = new VictorSPX(IntakeConstants.INTAKE_DROP_MOTOR_SPX);
   }
 
-  public void dropCollector(){
-    collectorDrop.set(true);
+  public void setIntaking(Boolean intaking) {
+    if (intaking) {
+      collectorDrop.set(true);
+      setSpeed(INTAKE_SPEED);
+    } else {
+      collectorDrop.set(false);
+      collector.disable();
+    }
   }
 
-  public void raiseCollector(){
-    collectorDrop.set(false);
-  }
-
-  public void runCollector(double collectorSpeed){
+  private void setSpeed(double collectorSpeed) {
     collector.set(ControlMode.PercentOutput, collectorSpeed);
   }
-
-  public void stopCollector(){
-    collector.set(ControlMode.PercentOutput, 0.0);
-  }
-
-  public void runIndexer(double indexerSpeed){
-    indexer.set(ControlMode.PercentOutput, indexerSpeed);
-  }
-
-  public void stopIndexer(){
-    indexer.set(ControlMode.PercentOutput, 0.0);
-  }
-
 }
