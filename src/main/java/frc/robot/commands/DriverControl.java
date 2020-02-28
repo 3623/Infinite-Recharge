73d205c4c00c5f84f3cd71899d4425831ad1c622
eric <erc.hakawa@gmail.com>
2020-02-28 12:17:47 -0500

    cleaned old vision crap

--- src/main/java/frc/robot/commands/DriverControl.java
+++ src/main/java/frc/robot/commands/DriverControl.java
@@ -10,22 +10,13 @@ package frc.robot.commands;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.Drivetrain;
 
 public class DriverControl extends CommandBase {
     private final Drivetrain Drive;
     private final DoubleSupplier m_forward;
     private final DoubleSupplier m_rotation;
-    NetworkTable Lime = NetworkTableInstance.getDefault().getTable("limelight"); // The Limelight Vision system posts
-                                                                                 // several useful bits
-                                                                                 // of data to Network Tables.
-    NetworkTableEntry tx = Lime.getEntry("tx"); // Horizontal Offset From Crosshair to Target (-27 to 27 degrees)
-    NetworkTableEntry tv = Lime.getEntry("tv");
 
     private final double kP = -0.1; // proportional control for target seeking
     private final double minCommand = 0.05; // apply minimum to make bot move as output approaches 0;
@@ -53,26 +44,15 @@ public class DriverControl extends CommandBase {
         else
             quickTurn = false;
 
-        //if (m_seekTarget.getAsBoolean()) {
-        //    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode")
-        //            .setNumber(ShooterConstants.LIMELIGHT_LED_FORCE_ON);
-        //} else {
-        //    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode")
-        //            .setNumber(ShooterConstants.LIMELIGHT_LED_FORCE_OFF);
-        //}
-
-        /*if (m_seekTarget.getAsBoolean() && tv.getDouble(0.0) > 0) {
-            double x = tx.getDouble(0.0);
-            double headingError = -tx.getDouble(0.0);
-            double steeringAdjust = 0.0;
-            if (x > 1.0) {
-                steeringAdjust = kP * headingError - minCommand;
-            } else if (x < -1.0) {
-                steeringAdjust = kP * headingError + minCommand;
-            }
-
-            Drive.terribleDrive(joystickY, joystickR + steeringAdjust, true);
-        } else*/ if (quickTurn) {
+        /*
+         * if (m_seekTarget.getAsBoolean() && tv.getDouble(0.0) > 0) { double x =
+         * tx.getDouble(0.0); double headingError = -tx.getDouble(0.0); double
+         * steeringAdjust = 0.0; if (x > 1.0) { steeringAdjust = kP * headingError -
+         * minCommand; } else if (x < -1.0) { steeringAdjust = kP * headingError +
+         * minCommand; }
+         *
+         * Drive.terribleDrive(joystickY, joystickR + steeringAdjust, true); } else
+         */ if (quickTurn) {
             Drive.terribleDrive(joystickY * 0.5, joystickR, quickTurn);
         } else {
             Drive.terribleDrive(joystickY * Math.abs(joystickY), joystickR * 0.5, quickTurn);
