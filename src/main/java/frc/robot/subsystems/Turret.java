eb0fab4824e7c17d40e19193c9f10c84155484c5
eric <erc.hakawa@gmail.com>
2020-02-29 00:49:10 -0500

    changed to wpilib pid

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.controller.PIDController;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
@@ -13,8 +15,80 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 import frc.robot.subsystems.*;
 
-public class Turret extends SubsystemBase {
+// public class Turret extends SubsystemBase {
+//     WPI_TalonSRX turretMotor;
+//     private static final double TICKS_PER_ENCODER_REV = 2048.0;
+//     private static final double ENCODER_REVS_PER_TURRET_REV = 18.0 / 196.0;
+
+//     private double MAX_GOAL = 180.0;
+//     private double MIN_GOAL = -180.0;
+
+//     private static final double kP = 1023.0 * 0.65 / (double) degreesToTalonUnits(360.0);
+//     private static final double kI = kP / 1000.0;
+//     private static final double kD = 0.01;
+//     private static final double DEADBAND = 1.5;
+
+//     private double setAngle = 0.0;
+
+//     public Turret() {
+//         turretMotor = new WPI_TalonSRX(Constants.ShooterConstants.SHOOTER_TURRET_MOTOR_SRX);
+//         turretMotor.configFactoryDefault();
+//         turretMotor.setInverted(false);
+//         turretMotor.setNeutralMode(NeutralMode.Brake);
+
+//         turretMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+//         turretMotor.configForwardSoftLimitThreshold(degreesToTalonUnits(MAX_GOAL), 0);
+//         turretMotor.configReverseSoftLimitThreshold(degreesToTalonUnits(MIN_GOAL), 0);
+//         turretMotor.configForwardSoftLimitEnable(true, 0);
+//         turretMotor.configReverseSoftLimitEnable(true, 0);
+
+//         turretMotor.configAllowableClosedloopError(0, (int) degreesToTalonUnits(DEADBAND));
+//         turretMotor.config_kF(0, 0.0);
+//         turretMotor.config_kP(0, kP);
+//         turretMotor.config_kD(0, kD);
+//         turretMotor.config_kI(0, kI);
+//     }
+
+//     public void monitor() {
+//         SmartDashboard.putNumber("Turret Angle", turretMotor.getSelectedSensorPosition());
+//         SmartDashboard.putNumber("Turret Output", (int) turretMotor.getMotorOutputPercent() * 100);
+//         SmartDashboard.putNumber("Turret Error", turretMotor.getClosedLoopError());
+//     }
+
+//     private static int degreesToTalonUnits(double degrees) {
+//         double turretRevs = degrees / 360.0;
+//         double encoderRevs = turretRevs * ENCODER_REVS_PER_TURRET_REV;
+//         double encoderTicks = encoderRevs * TICKS_PER_ENCODER_REV;
+//         return (int) encoderTicks; // BANANA check this
+//     }
+
+//     public Boolean setAngle(double angle) {
+//         setAngle = angle;
+//         turretMotor.set(ControlMode.Position, degreesToTalonUnits(setAngle));
+//         return atTarget();
+//     }
+
+//     public Double getAngle() {
+//         return setAngle; // BANANA TODO
+//     }
+
+//     private Boolean atTarget() {
+//         return (turretMotor.getClosedLoopError() < degreesToTalonUnits(DEADBAND));
+//     }
+
+//     public void zero() {
+//         turretMotor.setSelectedSensorPosition(0);
+//     }
+
+//     public void stop() {
+//         turretMotor.disable();
+//     }
+// }
+
+public class Turret extends PIDSubsystem {
     WPI_TalonSRX turretMotor;
+
+    private Encoder turretEncoder;
     private static final double TICKS_PER_ENCODER_REV = 2048.0;
     private static final double ENCODER_REVS_PER_TURRET_REV = 18.0 / 196.0;
 
@@ -26,32 +100,20 @@ public class Turret extends SubsystemBase {
     private static final double kD = 0.01;
     private static final double DEADBAND = 1.5;
 
-
     private double setAngle = 0.0;
 
     public Turret() {
+        super(new PIDController(kP, kI, kD));
+        getController().setTolerance(DEADBAND);
+
         turretMotor = new WPI_TalonSRX(Constants.ShooterConstants.SHOOTER_TURRET_MOTOR_SRX);
-        turretMotor.configFactoryDefault();
-        turretMotor.setInverted(false);
-        turretMotor.setNeutralMode(NeutralMode.Brake);
-
-        turretMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
-        turretMotor.configForwardSoftLimitThreshold(degreesToTalonUnits(MAX_GOAL), 0);
-        turretMotor.configReverseSoftLimitThreshold(degreesToTalonUnits(MIN_GOAL), 0);
-        turretMotor.configForwardSoftLimitEnable(true, 0);
-        turretMotor.configReverseSoftLimitEnable(true, 0);
-
-        turretMotor.configAllowableClosedloopError(0, (int) degreesToTalonUnits(DEADBAND));
-        turretMotor.config_kF(0, 0.0);
-        turretMotor.config_kP(0, kP);
-        turretMotor.config_kD(0, kD);
-        turretMotor.config_kI(0, kI);
+        turretEncoder.setDistancePerPulse(1.0 / degreesToTalonUnits(1.0));
     }
 
     public void monitor() {
-        SmartDashboard.putNumber("Turret Angle", turretMotor.getSelectedSensorPosition());
+        SmartDashboard.putNumber("Turret Angle", this.getAngle());
         SmartDashboard.putNumber("Turret Output", (int) turretMotor.getMotorOutputPercent() * 100);
-        SmartDashboard.putNumber("Turret Error", turretMotor.getClosedLoopError());
+        SmartDashboard.putNumber("Turret Error", getController().getPositionError());
     }
 
     private static int degreesToTalonUnits(double degrees) {
@@ -62,24 +124,34 @@ public class Turret extends SubsystemBase {
     }
 
     public Boolean setAngle(double angle) {
-        setAngle = degreesToTalonUnits(angle);
-        turretMotor.set(ControlMode.Position, setAngle);
+        setAngle = angle;
+        setSetpoint(angle);
         return atTarget();
     }
 
     public Double getAngle() {
-        return setAngle; // BANANA TODO
+        return this.getMeasurement(); // BANANA TODO
     }
 
     private Boolean atTarget() {
-        return (turretMotor.getClosedLoopError() < degreesToTalonUnits(DEADBAND));
+        return getController().atSetpoint();
     }
 
     public void zero() {
-        turretMotor.setSelectedSensorPosition(0);
+        turretEncoder.reset();
     }
 
     public void stop() {
         turretMotor.disable();
+    }
+
+    @Override
+    protected void useOutput(double output, double setpoint) {
+        turretMotor.set(ControlMode.PercentOutput, output);
+    }
+
+    @Override
+    protected double getMeasurement() {
+        return turretEncoder.getDistance();
     }
 }
